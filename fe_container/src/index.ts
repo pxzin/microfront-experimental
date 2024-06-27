32309b16571b7865c019ed7b7c614ccf6fb31909
Ricardo Martines <rjstock@gmail.com>
2024-06-27 20:32:50 -0300

    chore: setup inicial

---
+++ fe_container/src/index.ts
@@ -0,0 +1,6 @@
+import './styles.scss';
+
+const menuElement = document.getElementById('menu');
+if (menuElement) {
+  menuElement.innerText = 'Hello from menu Micro-Frontend with TypeScript';
+}
---
+++ fe_menu/src/index.ts
@@ -0,0 +1,6 @@
+import './styles.scss';
+
+const drawerElement = document.getElementById('drawer');
+if (drawerElement) {
+  drawerElement.innerText = 'Hello from Drawer Micro-Frontend with TypeScript';
+}
